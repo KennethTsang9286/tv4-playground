5698139207a7103047219bf7cea1d869526fc26a
Kenneth Tsang <Kenneth.Tsang@ofx.com>
2021-02-10 16:57:46 +1100

    main: Remove properties

--- index.js
+++ index.js
@@ -14,23 +14,6 @@ const schema = {
             fromAmount: { type: "number" },
             toCurrency: { type: "string" },
             toAmount: { type: "number" },
-            rate: { type: "number" },
-            inverseRate: { type: "number" },
-            fee: {
-              type: "object",
-              properties: {
-                amount: { type: "number" },
-                currency: { type: "string" },
-              },
-            },
-            freebies: {
-              type: "object",
-              properties: {
-                total: { type: "number" },
-                claimed: { type: "number" },
-                remaining: { type: "number" },
-              },
-            },
           },
         },
       },
@@ -41,22 +24,12 @@ const schema = {
 const response = {
   data: {
     createQuote: {
-      id: "c66887c7-594c-4989-81c5-7aa814938465",
+      id: "123123-123123-123123",
       fromCurrency: "USD",
       fromAmount: 79,
       toCurrency: "AUD",
       toAmount: 100,
-      rate: 1.2659,
-      inverseRate: 0.7899518129394107,
-      fee: {
-        amount: 5,
-        currency: "USD",
-      },
-      freebies: {
-        total: 0,
-        claimed: 0,
-        remaining: "0",
-      },
+      rate: 1,
     },
   },
 };
